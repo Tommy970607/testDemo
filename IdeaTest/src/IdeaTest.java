264b825dba6237006d9cbf1881e9e4821d2cd912
Tommy <zhxtang@foxmail.com>
2020-11-04 15:17:17 +0800

    test for folder

---
+++ IdeaTest/src/IdeaTest.java
@@ -0,0 +1,16 @@
+public class IdeaTest {
+    public static void main(String[] args) {
+        System.out.println("233333333");
+
+        for (long i = 0; i <1000000000 ; i++) {
+            for (long j = 0; j<1000000000 ; j++) {
+                for (long k = 0; k <1000000000 ; k++) {
+                    for (long m = 0; m <1000000000 ; m++) {
+                        System.out.println(m);
+                    }
+                }
+            }
+        }
+
+    }
+}
